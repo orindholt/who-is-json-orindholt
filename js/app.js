f57ced97ccc58c7317f3fe0820241603afce1dc5
orindholt <83012141+orindholt@users.noreply.github.com>
2021-09-10 10:04:48 +0200

    Remove unnecessary code and add improved js

--- js/app.js
+++ js/app.js
@@ -1,53 +1,47 @@
-const klasseKammerater = {
-    "kammerater": [
-        {
-            "fornavn": "Dejene",
-            "mellemnavn": "Daba",
-            "efternavn": "Jebassa",
-            "alder": 35,
-            "hårfarve": "sort"
-        },
-        {
-            "fornavn": "Nils",
-            "mellemnavn": "Sonne",
-            "efternavn": "Andersen",
-            "alder": 26,
-            "hårfarve": "brunt"
-        },
-        {
-            "fornavn": "Sebastian",
-            "mellemnavn": "Christopher",
-            "efternavn": "Andersen",
-            "alder": 18,
-            "hårfarve": "mørkebrunt"
-        },
-        {
-            "fornavn": "Troels",
-            "mellemnavn": "Heldt Agergaard",
-            "efternavn": "Rasmussen",
-            "alder": 35,
-            "hårfarve": "kasket"
-        }
-    ]
-};
+const team = [
+    {
+        "fornavn": "Dejene",
+        "mellemnavn": "Daba",
+        "efternavn": "Jebassa",
+        "alder": 35,
+        "hårfarve": "sort"
+    },
+    {
+        "fornavn": "Nils",
+        "mellemnavn": "Sonne",
+        "efternavn": "Andersen",
+        "alder": 26,
+        "hårfarve": "brunt"
+    },
+    {
+        "fornavn": "Sebastian",
+        "mellemnavn": "Christopher",
+        "efternavn": "Andersen",
+        "alder": 18,
+        "hårfarve": "mørkebrunt"
+    },
+    {
+        "fornavn": "Troels",
+        "mellemnavn": "Heldt Agergaard",
+        "efternavn": "Rasmussen",
+        "alder": 35,
+        "hårfarve": "kasket"
+    }  
+];
 
 let dataContainer = document.querySelector("#data");
 let dataList = document.createElement("ul");
 dataContainer.append(dataList);
 
-function createListItem(param){
-    for(let i = 0; i<klasseKammerater.kammerater.length; i++){
-        /* console.log(klasseKammerater.kammerater[i].navn.fornavn);
-        console.log(klasseKammerater.kammerater[i].navn.efternavn); */
+function createListItem(prop){
+    team.forEach(arrItem => {
         let dataListItem = document.createElement("li");
         dataList.append(dataListItem);
-        dataListItem.append(`${param}: ${klasseKammerater.kammerater[i][param]}`);
-    }
+        dataListItem.append(`${prop}: ${arrItem[prop]}`);
+    });
 }
 
 createListItem("fornavn");
 createListItem("efternavn");
 createListItem("alder");
 createListItem("hårfarve");
-
-
